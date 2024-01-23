e855da0cac6f4655b775ab2997aa3acc9fcd3f24
Nilesh <parkhenilesh@outlook.com>
2024-01-23 19:14:45 +0530

    JSDT005 - Create placeholders for scripts that call TF commands.

---
+++ scripts/tf-create.sh
@@ -0,0 +1,1 @@
+#TODO: Copy from gcloud-cli.sh while removing the unnecessary stuff.
---
+++ scripts/tf-update.sh
@@ -0,0 +1,1 @@
+#TODO: Simple script that checks if TF is installed, gcloud is authorized and then updates the resources created.
