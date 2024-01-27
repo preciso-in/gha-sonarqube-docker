54d9e78f2f194a5b79ccdbb3569b6dcb7b93a301
Nilesh <parkhenilesh@outlook.com>
2024-01-27 11:57:31 +0530

    JSDT005 - Change Terraform Outputs

--- terraform-config/outputs.tf
+++ terraform-config/outputs.tf
@@ -18,3 +18,23 @@ output "docker-url" {
   value     = "http://${google_compute_instance_from_template.docker.network_interface[0].access_config[0].nat_ip}:8085"
   sensitive = false
 }
+
+output "jenkins-server-name" {
+  value     = google_compute_instance_from_template.jenkins.name
+  sensitive = false
+}
+
+output "sonarquber-server-name" {
+  value     = google_compute_instance_from_template.sonarqube.name
+  sensitive = false
+}
+
+output "docker-server-name" {
+  value     = google_compute_instance_from_template.docker.name
+  sensitive = false
+}
+
+output "docker-server-ip" {
+  value     = google_compute_instance_from_template.docker.network_interface[0].access_config[0].nat_ip
+  sensitive = false
+}
