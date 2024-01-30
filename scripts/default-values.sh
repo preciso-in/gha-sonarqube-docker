155ef909782ed4e13725e2f16ac606b705b9272b
Nilesh <parkhenilesh@outlook.com>
2024-01-30 18:59:08 +0530

    GASD003 - Cleaned up repo keeping only website details

--- scripts/default-values.sh
+++
@@ -1,5 +0,0 @@
-project_name=jsd-with-terraform3
-
-export PROJECT_ID=$project_name
-export REGION=us-central1
-export BUCKET_ID="$project_name-state-bucket"
--- scripts/getInputs/process-arguments.sh
+++
@@ -1,8 +0,0 @@
-# Description: Asks arguments (project_id, region and bucket_id)
-# from command line if not provided as arguments
-# Arguments: $@
-# Returns: Configuration variables as environment variables
-
-handle_arguments() {
-	npx tsx ./getInputs/index.ts $@
-}
