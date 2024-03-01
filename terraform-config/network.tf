e6532f6cf12878f95e4fa8c152ad6025abcc2a39
Nilesh <parkhenilesh@outlook.com>
2024-03-01 14:31:49 +0530

    GASD011 - WIP Fix Superlinter & Terrascan errors
    GASD011 - WIP Register Workflow with GHA

--- terraform-config/network.tf
+++ terraform-config/network.tf
@@ -10,74 +10,74 @@ resource "google_compute_subnetwork" "subnetwork" {
   network       = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_firewall" "allow_icmp" {
-  name          = "allow-icmp"
-  direction     = "INGRESS"
-  priority      = 65534
-  network       = google_compute_network.vpc_network.id
-  source_ranges = ["0.0.0.0/0"]
+# resource "google_compute_firewall" "allow_icmp" {
+#   name          = "allow-icmp"
+#   direction     = "INGRESS"
+#   priority      = 65534
+#   network       = google_compute_network.vpc_network.id
+#   source_ranges = ["0.0.0.0/0"]
 
-  allow {
-    protocol = "icmp"
-  }
-}
+#   allow {
+#     protocol = "icmp"
+#   }
+# }
 
-resource "google_compute_firewall" "allow_internal" {
-  name          = "allow-internal"
-  direction     = "INGRESS"
-  priority      = 65534
-  network       = google_compute_network.vpc_network.id
-  source_ranges = ["10.128.0.0/9"]
+# resource "google_compute_firewall" "allow_internal" {
+#   name          = "allow-internal"
+#   direction     = "INGRESS"
+#   priority      = 65534
+#   network       = google_compute_network.vpc_network.id
+#   source_ranges = ["10.128.0.0/9"]
 
-  allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
-  }
-  allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
-  }
-  allow {
-    protocol = "icmp"
-  }
-}
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["0-65535"]
+#   }
+#   allow {
+#     protocol = "udp"
+#     ports    = ["0-65535"]
+#   }
+#   allow {
+#     protocol = "icmp"
+#   }
+# }
 
-resource "google_compute_firewall" "allow_rdp" {
-  name          = "allow-rdp"
-  direction     = "INGRESS"
-  priority      = 65534
-  network       = google_compute_network.vpc_network.id
-  source_ranges = ["0.0.0.0/0"]
+# # resource "google_compute_firewall" "allow_rdp" {
+# #   name          = "allow-rdp"
+# #   direction     = "INGRESS"
+# #   priority      = 65534
+# #   network       = google_compute_network.vpc_network.id
+# #   source_ranges = ["0.0.0.0/0"]
 
-  allow {
-    protocol = "tcp"
-    ports    = ["3389"]
-  }
-}
+# #   allow {
+# #     protocol = "tcp"
+# #     ports    = ["3389"]
+# #   }
+# # }
 
-resource "google_compute_firewall" "allow_ssh" {
-  name          = "allow-ssh"
-  direction     = "INGRESS"
-  priority      = 65534
-  network       = google_compute_network.vpc_network.id
-  source_ranges = ["0.0.0.0/0"]
+# # resource "google_compute_firewall" "allow_ssh" {
+# #   name          = "allow-ssh"
+# #   direction     = "INGRESS"
+# #   priority      = 65534
+# #   network       = google_compute_network.vpc_network.id
+# #   source_ranges = ["0.0.0.0/0"]
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-}
+# #   allow {
+# #     protocol = "tcp"
+# #     ports    = ["22"]
+# #   }
+# # }
 
-resource "google_compute_firewall" "allow_sonarqube" {
-  name          = "allow-sonarqube"
-  direction     = "INGRESS"
-  priority      = 1000
-  network       = google_compute_network.vpc_network.id
-  source_ranges = ["0.0.0.0/0"]
+# # resource "google_compute_firewall" "allow_sonarqube" {
+# #   name          = "allow-sonarqube"
+# #   direction     = "INGRESS"
+# #   priority      = 1000
+# #   network       = google_compute_network.vpc_network.id
+# #   source_ranges = ["0.0.0.0/0"]
 
-  allow {
-    protocol = "tcp"
-    ports    = ["9000"]
-  }
-  target_tags = [var.sonarqube_network_tag]
-}
+# #   allow {
+# #     protocol = "tcp"
+# #     ports    = ["9000"]
+# #   }
+# #   target_tags = [var.sonarqube_network_tag]
+# # }
